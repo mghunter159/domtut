e6e22d7a98057dc685613ff16a795d4bd962f47b
mghunter159 <ya@mghunter.ru>
2021-08-30 22:46:09 +0500

    add files

--- connect/constants.php
+++ connect/constants.php
@@ -3,5 +3,6 @@ $host = 'localhost'; // адрес сервера
 $database = 'domtut'; // имя базы данных
 $user = 'root'; // имя пользователя
 $password = 'SZ4F4Zm0reop'; // пароль
+$password = ''; // пароль
 
 ?>
